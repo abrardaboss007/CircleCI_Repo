e16af787d5421db22f30b452aa261f6288690176
Abrar Nadeem <abrarnadeem007@gmail.com>
2025-11-17 18:15:59 +0000

    Change code

--- calc.py
+++ calc.py
@@ -4,6 +4,6 @@ def triangular_numbers(n):
     if n < 1 or n != int(n):
         raise ValueError("Must be a positive integer")
     else:
-        return n * (n+1) // 2
+        return n * (n+1) / 2
     
 print(triangular_numbers(100))
